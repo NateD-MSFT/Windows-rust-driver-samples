f6d67798959b49345aebbcc6e19ef284cf09b4a2
NateD-MSFT <34494373+NateD-MSFT@users.noreply.github.com>
2024-06-13 16:39:35 -0500

    More basic driver setup

---
+++ usb/kmdf_fx2/driver/src/lib.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![cfg_attr(feature = "nightly", feature(hint_must_use))]
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+#![allow(clippy::missing_safety_doc)]
+
+#[cfg(not(test))]
+extern crate wdk_panic;
+
+
+#[cfg(not(test))]
+use wdk_alloc::WDKAllocator;
+use wdk_sys::{
+    DRIVER_OBJECT,
+    NTSTATUS,
+    PCUNICODE_STRING,
+};
+
+extern crate alloc;
+
+
+#[cfg(not(test))]
+#[global_allocator]
+static GLOBAL_ALLOCATOR: WDKAllocator = WDKAllocator;
+
+fn main() {}
+
+#[link_section = "INIT"]
+#[export_name = "DriverEntry"] // WDF expects a symbol with the name DriverEntry
+extern "system" fn driver_entry(
+    driver: &mut DRIVER_OBJECT,
+    registry_path: PCUNICODE_STRING,
+) -> NTSTATUS {
+    0
+}
